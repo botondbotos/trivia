d4f2ac29458e7228f2c6a1bd8ada3caf57d4dbf5
Botond Botos <botond.botos@gmail.com>
2018-03-24 11:08:53 +0100

    Golden master tests created.

---
+++ C#/Trivia/Trivia.Tests/GameRunnerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Trivia.UnitTests
+{
+    public class GameRunnerTests
+    {
+        //[Test]
+        //public void GenerateTestData()
+        //{
+        //    for (int i = 0; i < 100; i++)
+        //    {
+        //        using (var gameOutput = File.CreateText($@"TestData\GoldenData_{i}.txt"))
+        //        {
+        //            Console.SetOut(gameOutput);
+        //            GameRunner.Main(new[] { i.ToString() });
+
+        //            gameOutput.Flush();
+        //        }
+        //    }
+        //}
+
+        [Test]
+        public void GenerateGoldenSource([Range(0, 99)]int seed)
+        {
+            // Arrange
+            var gameOutputStringBuilder = new StringBuilder();
+            using (var gameOutput = new StringWriter(gameOutputStringBuilder))
+            {
+                Console.SetOut(gameOutput);
+
+                // Act 
+                GameRunner.Main(new[] { seed.ToString() });
+
+                // Assert
+                var actualGameOutput = gameOutputStringBuilder.ToString();
+                var expectedGameOutput = File.ReadAllText($@"TestData\GoldenData_{seed}.txt");
+                actualGameOutput.Should().BeEquivalentTo(expectedGameOutput);
+            }
+        }
+    }
+}
---
+++ C#/Trivia/Trivia.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Trivia.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Trivia.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cae5d510-edb7-48c6-a994-18f5c5d868ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
